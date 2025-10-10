89f9b92c6f372baf06b2eea84b1b1a8de1303315
cp-20 <n.naoki.1587@gmail.com>
2025-10-10 19:31:21 +0900

    画像を見せる exporter を作った

--- discord-exporter/bot.ts
+++ discord-exporter/bot.ts
@@ -1,13 +1,21 @@
-import { Client, Events, GatewayIntentBits, Message } from "discord.js";
+import {
+  Client,
+  Events,
+  GatewayIntentBits,
+  Message,
+  MessageFlags,
+} from "discord.js";
 import { typedEnv } from "./env";
 import {
   addMessageData,
   addPhotoData,
+  addReactionData,
   type AnnouncementData,
   removeReactionData,
   setInitialData,
 } from "./exporter";
 import { getUnicodeEmojiName } from "./emoji";
+import { addPhoto } from "./photo";
 
 const getEmojiName = (emoji: string | null): string => {
   return emoji ? (getUnicodeEmojiName(emoji) ?? emoji) : "unknown";
@@ -103,6 +111,26 @@ const syncState = async () => {
           message.author.tag,
           (photosMap.get(message.author.tag) ?? 0) + message.attachments.size,
         );
+        for (let i = 0; i < message.attachments.size; i++) {
+          const attachment = message.attachments.at(i);
+          if (attachment === undefined) continue;
+
+          const content = message.content.length > 50
+            ? `${message.content.slice(0, 50)}...`
+            : message.content;
+          let title = `${content} by ${message.author.tag}`;
+          if (message.attachments.size > 1) {
+            title += ` (${i + 1} / ${message.attachments.size})`;
+          }
+          addPhoto({
+            id: attachment.id,
+            title,
+            width: attachment.width,
+            height: attachment.height,
+            url: attachment.url,
+            createdAt: message.createdAt,
+          });
+        }
       }
     }
   }
@@ -150,6 +178,27 @@ export const startBot = async () => {
         user: message.author.tag,
         count: message.attachments.size,
       });
+      for (let i = 0; i < message.attachments.size; i++) {
+        const attachment = message.attachments.at(i);
+        if (attachment === undefined) continue;
+
+        const content = message.content.length > 50
+          ? `${message.content.slice(0, 50)}...`
+          : message.content;
+        let title = `${content} by ${message.author.tag}`;
+        if (message.attachments.size > 1) {
+          title += ` (${i + 1} / ${message.attachments.size})`;
+        }
+
+        addPhoto({
+          id: attachment.id,
+          title,
+          width: attachment.width,
+          height: attachment.height,
+          url: attachment.url,
+          createdAt: message.createdAt,
+        });
+      }
     }
   });
 
@@ -173,7 +222,7 @@ export const startBot = async () => {
     }
 
     const emojiName = getEmojiName(reaction.emoji.name);
-    addMessageData({ user: user.tag, count: 1 });
+    addReactionData({ user: user.tag, emoji: emojiName, count: 1 });
     console.log(
       `Reaction added: :${emojiName}: by ${user.tag} on message ${reaction.message.id}`,
     );
